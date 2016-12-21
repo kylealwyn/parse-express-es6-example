dac177c91c567a2d512d88ca8a063cb3cd7d02e7
Kyle Alwyn <kjalwyn@gmail.com>
2016-12-20 23:34:36 -0800

    comments

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ import { ParseServer } from 'parse-server';
 
 import Constants from './constants';
 
+// Create Express App
 const app = express();
 
 // Create and mount Parse server
 const parseServer = new ParseServer(Constants.ParseConfig)
 app.use(Constants.ParseServerMount, parseServer);
 
-// Mount webhooks
-// console.log(ParseCloud);
-// app.use(Constants.ParseWebhookMount, ParseCloud.app)
-//
+// Mount Express app
 app.listen(Constants.Port, (err) => {
   if (err) {
     throw err;
